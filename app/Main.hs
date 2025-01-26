22eadb20d4ab2421fafcb99864459473994f3900
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-01-26 21:00:19 +0000

    Add example render

--- app/Main.hs
+++ app/Main.hs
@@ -10,27 +10,30 @@ module Main
 import           Control.Monad ( void )
 import           Data.GI.Base ( AttrOp (..), new, on )
 import           Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import           Data.Text ( Text )
+import qualified GI.Cairo.Render as Cairo
+import qualified GI.Cairo.Render.Connector as Cairo
 import qualified GI.Gtk as Gtk
+import qualified GI.Pango as Pango
+import qualified GI.PangoCairo as Pango
 
 data AppState = AppState
   { appPageSetup :: Gtk.PageSetup
   , appPrintSettings :: Gtk.PrintSettings
   }
 
-main :: IO ()
-main = do
-  pageSetupInit <- Gtk.pageSetupNew
-  printSettingsInit <- Gtk.printSettingsNew
-  let appState = AppState
-        { appPageSetup = pageSetupInit
-        , appPrintSettings = printSettingsInit
-        }
-  appStateRef <- newIORef appState
-  app <- new Gtk.Application
-    [ #applicationId := "com.pilgrem.gtk-print"
-    , On #activate (activate ?self appStateRef)
-    ]
-  void $ app.run Nothing
+exampleRender :: Cairo.Render ()
+exampleRender = do
+  Cairo.moveTo 72 72
+  Cairo.setFontSize 10
+  Cairo.showText ("Hello, Printer!" :: Text)
+  Cairo.moveTo 72 144
+  Cairo.toRender $ \context -> do
+    layout <- Pango.createLayout context
+    #setText layout "Hello, Printer!" (-1)
+    fontDescription <- Pango.fontDescriptionFromString "Cambria 14"
+    #setFontDescription layout (Just fontDescription)
+    Pango.showLayout context layout
 
 activate :: Gtk.Application -> IORef AppState -> IO ()
 activate app appStateRef = do
@@ -40,8 +43,21 @@ activate app appStateRef = do
     , #resizable := False
     ]
 
+  box <- new Gtk.Box
+    [ #orientation := Gtk.OrientationHorizontal
+    , #spacing := 5
+    , #marginTop := 5
+    , #marginBottom := 5
+    , #marginStart := 5
+    , #marginEnd := 5
+    , #halign := Gtk.AlignCenter
+    ]
+
   pageSetupButton <- new Gtk.Button
     [ #label := "Page setup" ]
+
+  printButton <- new Gtk.Button
+    [ #label := "Print" ]
 
   let onClickPageSetup :: IO ()
       onClickPageSetup = do
@@ -59,8 +75,50 @@ activate app appStateRef = do
         writeIORef appStateRef $ appState
           { appPageSetup = pageSetup }
 
-  void $ on pageSetupButton #clicked onClickPageSetup
+  let onClickPrint :: IO ()
+      onClickPrint = do
+        appState <- readIORef appStateRef
+        printOperation <- new Gtk.PrintOperation
+          [ #nPages := 1
+          , #unit := Gtk.UnitPoints
+          , #defaultPageSetup := appPageSetup appState
+          , #printSettings := appPrintSettings appState
+          , On #drawPage onDrawPage
+          ]
+        void $
+          #run printOperation Gtk.PrintOperationActionPrintDialog (Just window)
+        mPrintSettings <- #getPrintSettings printOperation
+        case mPrintSettings of
+          Nothing -> pure ()
+          Just printSettings ->
+            writeIORef appStateRef $ appState
+              { appPrintSettings = printSettings }
 
-  #setChild window (Just pageSetupButton)
+  void $ on pageSetupButton #clicked onClickPageSetup
+  void $ on printButton #clicked onClickPrint
+
+  #append box pageSetupButton
+  #append box printButton
+  #setChild window (Just box)
 
   window.show
+
+onDrawPage :: Gtk.PrintOperationDrawPageCallback
+onDrawPage printContext _pageNr = do
+  context <- #getCairoContext printContext
+  Cairo.renderWithContext exampleRender context
+
+main :: IO ()
+main = do
+  pageSetupInit <- Gtk.pageSetupNew
+  printSettingsInit <- Gtk.printSettingsNew
+  let appState = AppState
+        { appPageSetup = pageSetupInit
+        , appPrintSettings = printSettingsInit
+        }
+  appStateRef <- newIORef appState
+  app <- new Gtk.Application
+    [ #applicationId := "com.pilgrem.gtk-print"
+    , On #activate (activate ?self appStateRef)
+    ]
+  void $ app.run Nothing
